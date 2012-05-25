dd0591418952403378b46259fb4cb03248f35968
Mitchell Durham <mitchelldurham@mitchelldurham.com>
2012-05-24 22:51:54 -0400

    changed player to a dynamic body

--- src/Player.java
+++ src/Player.java
@@ -1,7 +1,7 @@
 import java.awt.Graphics;
 
-import org.jbox2d.collision.shapes.CircleShape;
 import org.jbox2d.collision.shapes.PolygonShape;
+import org.jbox2d.common.Vec2;
 import org.jbox2d.dynamics.Body;
 import org.jbox2d.dynamics.BodyDef;
 import org.jbox2d.dynamics.BodyType;
@@ -13,16 +13,12 @@ public class Player {
 	
 	private static final float RATE = 30;
 	private Body body;
-	private int x;
-	private int y;
-	private World world;
 	
 	Player(World world)
 	{
-		this.world = world;
 		
 		BodyDef bodyDef = new BodyDef();
-	    bodyDef.type = BodyType.STATIC;
+	    bodyDef.type = BodyType.DYNAMIC;
 	    bodyDef.fixedRotation = true;
 	    bodyDef.position.set(20.0f/RATE, 375.0f/RATE);
 	    body = world.createBody(bodyDef);
@@ -32,18 +28,18 @@ public class Player {
 	    
 	    FixtureDef fixtureDef = new FixtureDef();
 	    fixtureDef.shape = box;
-	    fixtureDef.restitution = 0.8f;
 	    fixtureDef.density = 2.0f;
 	    fixtureDef.friction = 0.3f;
 	    body.createFixture(fixtureDef);
 		
-		
+	    body.setLinearVelocity(new Vec2(5.0f,0.0f));
 	}
 	
 	public void draw (Graphics g)
 	{
-		g.drawRect(10, 370, 20, 30);
-		g.drawOval (10, 350, 20, 20);
+		Vec2 position = body.getPosition();
+		g.drawRect((int) (position.x*RATE - 10), 370, 20, 30);
+		g.drawOval ((int) (position.x*RATE - 10), 350, 20, 20);
 	}
 
 }
